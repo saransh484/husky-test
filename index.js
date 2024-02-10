18bcd9412e9c53308b8a931fd85950650598deeb
Saransh Bhatnagar <saransh8@hotmail.com>
2024-02-10 10:55:58 +0530

    test

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const uri = 'mongodb://localhost:27017'
+
+// mongoose.connect(uri)
+
+// const conn = mongoose.connection;
+
+// conn.on('open',()=>{
+//   console.log('DB Connected')
+// })
+
+const app = express()
+
+const port = 5000
+
+// app.use(cors({
+//     origin: '*'
+// }))
+
+app.use(express.json());
+
+app.use("/api", (req,res)=>{
+    res.send('Hello World')
+});
+
+app.listen(port, () => {
+  console.log(`Listening to Port : ${port}`);
+});
